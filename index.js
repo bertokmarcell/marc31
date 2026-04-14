f7dc766e818c2ff1e09852527e6ca2bde5b818b9
bertokmarcell <bertok.marcell@diak.szamalk-szalezi.hu>
2026-04-14 13:22:24 +0200

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ import Kepek from "./kepek.js";
 import Kep from "./kep.js";
 const kepekTaroloELEM = document.querySelector(".kepek-tarolo");
 const nagyKep= document.querySelector(".nagy-kep");
+const viszgomb=document.querySelector(".gomb0");
+const elogomb=document.querySelector(".gomb1");
+let currentindex=0;
 new Kepek(KEPEK_LISTA, kepekTaroloELEM);
 let nagyKepPeldany = new Kep(KEPEK_LISTA[0], nagyKep);
 window.addEventListener("kiválasztott", function (event) {
     nagyKepPeldany.megjelenit(event.detail);
+});
+viszgomb.addEventListener("click",function(){
+    console.log(currentindex)
+    if(currentindex==0){currentindex+=4}
+    currentindex=(currentindex-1)
+    nagyKepPeldany.megjelenit(KEPEK_LISTA[currentindex])
+    
+});
+elogomb.addEventListener("click",function(){
+    console.log(currentindex)
+    currentindex=(currentindex+1)
+    if(currentindex==4){currentindex=0}
+    nagyKepPeldany.megjelenit(KEPEK_LISTA[currentindex])
 });
